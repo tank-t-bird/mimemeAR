5ae7402fd93f85351fdfa69eeda1be60e1b1d237
Tank Thunderbird <tank.thunderbird@gmail.com>
2017-10-05 13:09:12 +0200

    initial commit after minor cleanup in BItbucket Rep

---
+++ Assets/HeadObjectController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class HeadObjectController : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	private string whoAmI;
+	private GameObject theChild;
+	public GameObject HeadPiece;
+
+
+	void Start () {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED)
+		{
+			whoAmI = gameObject.name;
+
+			if (whoAmI == "Arrival")
+			{
+				theChild = HeadPiece.transform.Find("Arrive").gameObject;
+				theChild.SetActive(true);
+		
+			} 
+
+			else if (whoAmI == "Attraction")
+			{
+				theChild = HeadPiece.transform.Find("Attract").gameObject;
+				theChild.SetActive(true);
+		
+			}
+			else if (whoAmI == "Intimacy")
+			{
+				theChild = HeadPiece.transform.Find("Intimate").gameObject;
+				theChild.SetActive(true);
+
+			}
+			else if (whoAmI == "Departure")
+			{
+				theChild = HeadPiece.transform.Find("Depart").gameObject;
+				theChild.SetActive(true);
+
+			}
+		}
+		else
+		{
+			//foreach (Transform child in transform)
+
+			//foreach (Transform child in HeadPiece.transform) 
+			
+			//{ HeadPiece.SetActive(false); }
+
+			theChild = HeadPiece.transform.Find("Arrive").gameObject;
+			theChild.SetActive(false);
+			theChild = HeadPiece.transform.Find("Attract").gameObject;
+			theChild.SetActive(false);
+			theChild = HeadPiece.transform.Find("Intimate").gameObject;
+			theChild.SetActive(false);
+			theChild = HeadPiece.transform.Find("Depart").gameObject;
+			theChild.SetActive(false);
+		}
+	}
+		
+}
+
+	
+
+	
+
+
+
+	
---
+++ Assets/Scripts/Autofocus.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Vuforia;
+
+public class Autofocus : MonoBehaviour {
+
+
+	void Start () 
+	{
+		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		VuforiaARController.Instance.RegisterOnPauseCallback(OnPaused);
+	}
+
+	private void OnVuforiaStarted()
+	{
+
+
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused)
+	{
+		if (!paused) // resumed
+		{
+			// Set again autofocus mode when app is resumed
+			CameraDevice.Instance.SetFocusMode(
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		}
+	}
+
+	void onEnable(){
+		bool focusModeSet = CameraDevice.Instance.SetFocusMode( 
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		//CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO;
+		if (!focusModeSet) {
+			Debug.Log("Failed to set focus mode (unsupported mode).");
+		}
+	}
+}
+
